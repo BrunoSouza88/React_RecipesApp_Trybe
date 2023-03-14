dbec074fd5b01e021d12db3051b867c44adc744c
BrunoSouza <desouza88@hotmail.com>
2023-03-14 16:43:26 -0300

    Import useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 // import rockGlass from './images/rockGlass.svg';
